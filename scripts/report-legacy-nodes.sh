5aa5292a5a7cd53589f6fc8eee163a5ff7a66ff7
Igor Bubelov <igor@bubelov.com>
2022-09-14 21:14:14 +0700

    Drop the to

--- scripts/report-legacy-nodes.sh
+++ scripts/report-legacy-nodes.sh
@@ -7,6 +7,6 @@ fi
 
 nodes=$(jq 'length' < /srv/http/data.btcmap.org/legacy-nodes.json)
 
-message="We still have $nodes legacy nodes. Help us to verify and update them: https://data.btcmap.org/legacy-nodes.html"
+message="We still have $nodes legacy nodes. Help us verify and update them: https://data.btcmap.org/legacy-nodes.html"
 
 curl -H "Content-Type: application/json" -d "{\"username\": \"btcmap.org\", \"content\": \"$message\"}" "${DISCORD_WEBHOOK_URL}"
