b9807d90cc7281caefd5e4f1c34266992f77a57e
Igor Bubelov <igor@bubelov.com>
2022-10-20 20:56:45 +0700

    Update config

--- scripts/generate-legacy-nodes.sh
+++ scripts/generate-legacy-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jq '[.elements[] | { id, type, name: .tags.name, legacy: .tags."payment:bitcoin" } | select(.type | test("node")) | select(.legacy | test("yes")?) + {"edit_link":("https://www.openstreetmap.org/edit?node=" + (.id | tostring))}]' < /srv/http/data.btcmap.org/elements.json > /srv/http/data.btcmap.org/legacy-nodes.json
+curl -s https://api.btcmap.org/v2/elements | jq '[.[] | { id, type: .osm_json.type, name: .osm_json.tags.name, legacy: .osm_json.tags."payment:bitcoin", deleted_at: .deleted_at } | select(.type | test("node")) | select(.legacy | test("yes")?) | select(.deleted_at == "") + {"edit_link":("https://www.openstreetmap.org/edit?node=" + (.id | tostring))}]' > /srv/http/data.btcmap.org/legacy-nodes.json
 
 html=/srv/http/data.btcmap.org/legacy-nodes.html
 echo '<!DOCTYPE html>' > $html
